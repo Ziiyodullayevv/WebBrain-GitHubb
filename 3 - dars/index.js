7dd3d94208057fcfa3de353538c046b4f7dab6e0
Akobir <akobirziyod@gmail.com>
2023-03-02 09:58:17 +0500

    If else, Nullish

---
+++ 3 - dars/index.js
@@ -0,0 +1,44 @@
+// 1 - masala:
+if ('0') {
+    console.log('Hello') // Hello chiqadi
+}
+
+// 2 - masala:
+
+let brand = 'ECMAScrip';
+
+if (brand === 'ECMAScript') console.log('Right!');
+else console.log('You dont now ECMAScript');
+
+// 3 - masala:
+let num = -1;
+if (num === 0) console.log('0 kiritdingiz');
+else if (num >= 1) console.log('1 ga teng va undan dan yuqori raqam');
+else console.log('-1 ga teng va undan kichik');
+
+// 4 - masala:
+
+let a = 2;
+let b = 3;
+let result;
+
+let c = (a + b < 4) ? result = "Below"  : result = "Over"
+console.log(result);
+
+
+// 5 - masala:
+let login = 'Employee'
+let message;
+
+login == 'Employee' ? message = 'Hello'
+: (login == "Director") 
+? message = 'Greetings'
+: (login == '') 
+? message = 'No login'
+: message = ''
+
+console.log(message);
+
+console.log(Number(' \t' * 2));
+
+
