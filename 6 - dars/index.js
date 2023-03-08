7b759476f802aa1128781faaa7ba9eac46c1357b
Ziiyodullayevv <akobir2323@gmail.com>
2023-03-08 23:04:09 +0500

    object ichidagi eng yuqori maoshni hisoblash

--- 6 - dars/index.js
+++ 6 - dars/index.js
@@ -6,4 +6,32 @@ const user = {
 user.name = "Pete";
 delete user.name;
 
+//-----------------------------------------------------------------------------------------
+
+let schedule = {};
+
+function isEmpty(obj) {
+  let key = Object.keys(obj).length;
+
+  if (key) return false;
+  else return true;
+}
+console.log(isEmpty(schedule));
+
+//-----------------------------------------------------------------------------------------
+
+//Eng kop maosh oladiganini aniqlang?
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+function getMaxManey(obj) {
+  let arr = Object.values(obj);
+  return Math.max(...arr);
+}
+console.log(getMaxManey(salaries));
+
 //------------------------------------------------------------------------------------------
