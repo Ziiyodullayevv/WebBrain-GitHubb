ff74f368a3321960a8297ba50f8b82f2d91db742
Ziiyodullayevv <akobir2323@gmail.com>
2023-03-17 18:31:40 +0500

    Strigns

--- 9 - dars/index.js
+++ 9 - dars/index.js
@@ -3,20 +3,17 @@
 // 1) check(str1, str2) funksiya yozilsin. Berilgan parametorlar bir-biriga mos yoki umuman mos kelmasligi va qisman mos ekanligini ifodalovchi natija qaytarsin
 
 function check(str1, str2) {
-  let string = new String(str2);
-  let compare = str1.localeCompare(string);
-  if (compare == 1) return "qisman togri";
-  else if (compare == -1) return "Mutlaqo teng emas";
-  else return "100% teng";
+  if (str1 === str2) return "100% teng";
+  else if (str1.includes(str2)) return "Qisman teng";
+  else return "Mutlaqo teng emas";
 }
-let compare = check("webbrain", "jscd");
-// console.log(compare);
-console.log(undefined++);
+// console.log(check("webbrain", "webbrain"));
 
 // -------------------------------------------------------------------------
 
 // 2) getCount(str) - function yozilsin. function str dagi harflar sanogini chiqarib bering?
-// Chiqishi str = 'webbrain' --> {w: }
+// Chiqishi str = 'webbrain'; --> { w:1, e:1, b:2, r:1, a:1, i:1, n: 1 };
+
 const getCount = (str) => {
   let count = {};
   for (let i = 0; i < str.length; i++) {
@@ -37,7 +34,7 @@ const truncate = (str, n) => {
 
 // -------------------------------------------------------------------------
 
-// 4) getCurrent(str) berilgan pul birligidan faqat raqamlarini ajrating
+// 4) getCurrent(str) berilgan pul birligidan faqat raqamlarini ajrating. eslatma pul birliklari turlicha bolishi mumkin va ular faqat soz boshida yoki sozning oxirida keladi.
 
 const getCurrent = (str) => {
   let res = "";
@@ -47,7 +44,11 @@ const getCurrent = (str) => {
   }
   return +res;
 };
-// console.log(getCurrent('$1200'));
+// console.log(getCurrent('1200')); // xatoo
+
+// -------------------------------------------------------------------------
+
+// 5)
 
 // -------------------------------------------------------------------------
 
@@ -62,3 +63,42 @@ const capitilize = (str) => {
   return res.trim();
 };
 // console.log(capitilize("hello world"));
+
+// =========
+
+// 7) 'webbrain' kabi istalgan matn berilgan bolsa uni 'wweebbbbrraaiinn' korinishida chiqaring.
+
+function double(str) {
+  let res = "";
+  for (value of str) {
+    res += value + value;
+  }
+  return res;
+}
+// console.log(double("web"));
+
+// ---------------------------------------------------------------------
+
+// 8) berilgan soz palendrom yoki palendrom emasligini aniqlovchi function tuzish kerak
+
+const polendrom = (str) => {
+  let res = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    res += str[i];
+  }
+  return res === str;
+};
+// console.log(polendrom("nonun"));
+
+// ----------------------------------------------------------------------
+
+// 9) string berilgan uni ziplash kerak?
+// Chiqishi: 'weeebbbrain academyyyy' --> 'we3b3rain academy4'
+
+const getZipper = (str) => {
+  let count = {};
+  for (value of str) {
+  }
+  return;
+};
+console.log(getZipper("webbrain"));
