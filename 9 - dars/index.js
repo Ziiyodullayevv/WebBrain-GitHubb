a1f89e15d919977b6149907d3f995bb12e5cec0f
Ziiyodullayevv <akobir2323@gmail.com>
2023-03-18 15:52:04 +0500

    Strings

--- 9 - dars/index.js
+++ 9 - dars/index.js
@@ -106,3 +106,21 @@ const polendrom = (str) => {
 
 // 9) string berilgan uni ziplash kerak?
 // Chiqishi: 'weeebbbrain academyyyy' --> 'we3b3rain academy4'
+
+const text = (str) => {
+  let res = "";
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      res += str[i];
+      if (count > 1) {
+        res += count;
+        count = 1;
+      }
+    }
+  }
+  return res;
+};
+console.log(text("wwbbraaaaain"));
