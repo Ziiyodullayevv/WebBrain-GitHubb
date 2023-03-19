43b2ae4e053d8a3ce8705c660a7294e8fd10fb3a
Ziiyodullayevv <akobir2323@gmail.com>
2023-03-19 20:55:15 +0500

    Array Introducdion 1/3

--- 10 - dars/index.js
+++ 10 - dars/index.js
@@ -66,3 +66,16 @@ const getChar = (arr) => {
 console.log(getChar(arrStr));
 
 // --------------------------------------------------------------------
+
+// 3 - Masala
+// Arrayni bir-biriga taqqoslang?
+
+const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const num2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const isEqual = (arr1, arr2) => {
+  return arr1.join("") === arr2.join("");
+};
+console.log(isEqual(num1, num2));
+
+// --------------------------------------------------------------------
