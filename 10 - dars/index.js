e804833418959fdfe9a342c7947ba2d0b03e2ba7
Ziiyodullayevv <akobir2323@gmail.com>
2023-03-21 21:16:28 +0500

    Array Introduction 1/3

--- 10 - dars/index.js
+++ 10 - dars/index.js
@@ -96,3 +96,30 @@ const camelize = (arr) => {
 // console.log(camelize(lowerCase));
 
 // --------------------------------------------------------------------
+
+// (5) Advensed Masala:
+
+let users = [
+  { id: 1, year: 1998, engine: 1, name: "Tiger" },
+  { id: 2, year: 2005, engine: 1.2, name: "Matiz" },
+  { id: 3, year: 2010, engine: 1.6, name: "Gentra" },
+  { id: 4, year: 2010, engine: 1.5, name: "Cobalt" },
+  { id: 5, year: 2012, engine: 2, name: "Malibu" },
+  { id: 6, year: 2000, engine: 1.2, name: "Damas" },
+  { id: 7, year: 2018, engine: 2.4, name: "Aracker" },
+];
+
+// Alifbo boyicha tartiblash:
+
+const tartiblash = (arr) => {
+  let res = arr.sort((a, b) => a.name.localeCompare(b.name));
+  users = res;
+};
+tartiblash(users);
+console.log(users);
+
+let nu = [1, 2, 3];
+let res = nu.map((value) => console.log(value));
+// console.log(res);
+
+// -------------------------------------------------------------------
