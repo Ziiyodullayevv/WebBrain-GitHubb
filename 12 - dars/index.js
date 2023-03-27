2cf72bf9f233ed9364569a6f98603d7cb23a778e
Ziiyodullayevv <akobir2323@gmail.com>
2023-03-28 00:30:27 +0500

    Advenced Array CRUD 3/3

--- 12 - dars/index.js
+++ 12 - dars/index.js
@@ -123,7 +123,7 @@ const getStrCharNum = (str) => {
 
   return res;
 };
-console.log(getStrCharNum(str8));
+// console.log(getStrCharNum(str8));
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -168,6 +168,55 @@ const getObjKeys = (obj) => {
   }
 };
 
-console.log(getObjKeys(obj1));
+// console.log(getObjKeys(obj1));
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// 2 - Masala:
+// Ojbect ichidagi agelar yigindisini toping ?
+
+let obj2 = {
+  title: "Webbrain",
+  age: 2,
+  child: {
+    age: 4,
+    name: "Webbrain",
+    child: {
+      age: 23,
+    },
+  },
+};
+
+const getObjAgeTotal = (obj) => {
+  let res = 0;
+
+  while (obj) {
+    res += obj.age;
+    obj = obj.child;
+  }
+
+  return res;
+};
+console.log(getObjAgeTotal(obj2));
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// 3 - Masala:
+// Object ichidagi Boolean tipidagi malumotlarni chiqaring keylari bilan birga?
+
+let obj = {
+  id: 1,
+  name: "webbrain",
+  offline: true,
+  online: true,
+  individual: false,
+};
+
+const getBoolean = (obj) => {
+  let res = ``;
+  for (key in obj) {
+    if (typeof obj[key] === "boolean") res += ` ${key} : ${obj[key]}; `;
+  }
+  return res;
+};
+// console.log(getBoolean(obj));
