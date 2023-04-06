898d25fbbdea5c905fcc9ebf99bf1c5bdc6c8218
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-06 11:55:06 +0500

    Recursion

---
+++ 15 - dars/index.js
@@ -0,0 +1,28 @@
+
+let sum = (n) => {
+    if(n === 1) return 1;
+    else {
+        return n *= sum(n - 1);
+    }
+}
+// console.log(sum(5));
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+const fibonacci = (n) => {
+    let fibonacci;
+    let fib = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+        fibonacci = fib[i]
+    };
+
+    return fibonacci;
+
+}
+console.log(fibonacci(7));
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
