a14458510b7f4c2447fa7d774687e753bd2d57e4
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-08 12:14:29 +0500

    Recursion function

--- 15 - dars/index.js
+++ 15 - dars/index.js
@@ -1,30 +1,50 @@
 
-let sum = (n) => {
-    if(n === 1) return 1;
-    else {
-        return n *= sum(n - 1);
-    }
-}
-// console.log(sum(5));
+// let sum = (n) => {
+//     if(n === 1) return 1;
+//     else {
+//         return n *= sum(n - 1);
+//     }
+// }
+// // console.log(sum(5));
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
-const fibonacci = (n) => {
-    let fibonacci;
-    let fib = [0, 1];
+// const fibonacci = (n) => {
+//     let fibonacci;
+//     let fib = [0,1];
 
-    for (let i = 2; i <= n; i++) {
-        fib[i] = fib[i - 2] + fib[i - 1];
-        fibonacci = fib[i]
-    };
+//     for (let i = 2; i <= n; i++) {
+//         fib[i] = fib[i - 2] + fib[i - 1];
 
-    return fibonacci;
+//         console.log( fib[i], i);
+//         fibonacci = fib[i]
+//     };
 
-}
-console.log(fibonacci(7));
+//     return fibonacci;
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// };
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let list = [
+    {id: 1, name: "eshmat", title: 'seo'},
+    {id: 2, name: 'toshmat', title: 'leo'},
+    {id: 3, name: "eshmat", title: 'seo'},
+    {id: 5, name: 'aqlbek', title: 'leo'},
+    {id: 4, name: 'shosha', title:'leo'}
+];
+
+// Output: 
+
+let res = {
+    seo: [
+        {id: 1, name: "eshmat", title: 'seo'},
+        {id: 2, name: 'toshmat', title: 'leo'},
+    ],
+    leo: [
+        {id: 2, name: 'toshmat', title: 'leo'},
+        {id: 5, name: 'aqlbek', title: 'leo'},
+    ]
+};
+
+
