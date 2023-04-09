caa8a1ae0dc5df43e29f2a1c60b3f9b4e1c9fe71
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-09 23:27:02 +0500

    DDOS Attack

--- 15 - dars/index.js
+++ 15 - dars/index.js
@@ -7,7 +7,7 @@
 // }
 // // console.log(sum(5));
 
-// // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
@@ -26,24 +26,82 @@
 
 // };
 
-let list = [
-    {id: 1, name: "eshmat", title: 'seo'},
-    {id: 2, name: 'toshmat', title: 'leo'},
-    {id: 3, name: "eshmat", title: 'seo'},
-    {id: 5, name: 'aqlbek', title: 'leo'},
-    {id: 4, name: 'shosha', title:'leo'}
-];
+// let list = [
+//     { id: 1, name: "eshmat", title: 'seo' },
+//     { id: 2, name: 'toshmat', title: 'leo' },
+//     { id: 3, name: "eshmat", title: 'seo' },
+//     { id: 5, name: 'aqlbek', title: 'leo' },
+//     { id: 4, name: 'shosha', title: 'leo' }
+// ];
 
-// Output: 
+// // Output: 
 
-let res = {
-    seo: [
-        {id: 1, name: "eshmat", title: 'seo'},
-        {id: 2, name: 'toshmat', title: 'leo'},
-    ],
-    leo: [
-        {id: 2, name: 'toshmat', title: 'leo'},
-        {id: 5, name: 'aqlbek', title: 'leo'},
-    ]
+// let resCount = {
+//     seo: [
+//         { id: 1, name: "eshmat", title: 'seo' },
+//         { id: 2, name: 'toshmat', title: 'leo' },
+//     ],
+//     leo: [
+//         { id: 2, name: 'toshmat', title: 'leo' },
+//         { id: 5, name: 'Toshmat', title: 'leo' },
+//     ]
+// };
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// DDOS attack:
+
+// function getData() {
+//     return fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json());
+// }
+
+// function getCached(getData) {
+//     let cache = new Map();
+
+//     return function (key) {
+//         if (cache.has(key)) {
+//             console.log(cache.get(key));
+//             return cache.get(key);
+//         } else {
+//             let res = getData();
+//             return cache.set(key, res);
+//         }
+//     }
+// }
+
+// getData = getCached(getData);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Demanik tarzdagi malumotni DDOS Atackdan himoyalash:
+
+let users = {
+    companyName: 'users',
+    getData() {
+        return fetch(`https://jsonplaceholder.typicode.com/${this.companyName}`)
+            .then((response) => response.json());
+    }
 };
+console.log(users.getData());
+function getCached(getData) {
+    let cache = new Map();
+
+    return function (key) {
+        if (cache.has(key)) {
+            console.log(cache.get(key));
+            return cache.get(key);
+        } else {
+            let res = getData.call(this);// this ni qaytarib qoydik;
+            return cache.set(key, res);
+        }
+    }
+}
+
+// users.getData = getCached(users.getData)
+users.getData() // Bu qism buttonga biriktirilishi kerak aslida;
+
+
+
 
