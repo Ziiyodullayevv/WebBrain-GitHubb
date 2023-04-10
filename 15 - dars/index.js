685913f2120baf3395560153d22aadaa46a2e567
Ziiyodullayevv <akobirziyod2323@gmail.com>
2023-04-10 18:03:55 +0500

    setters and getters

--- 15 - dars/index.js
+++ 15 - dars/index.js
@@ -77,33 +77,85 @@
 
 // Demanik tarzdagi malumotni DDOS Atackdan himoyalash:
 
-let users = {
-    companyName: 'users',
-    getData() {
-        return fetch(`https://jsonplaceholder.typicode.com/${this.companyName}`)
-            .then((response) => response.json());
-    }
-};
-console.log(users.getData());
-function getCached(getData) {
-    let cache = new Map();
+// let users = {
+//     companyName: 'users',
+//     getData() {
+//         return fetch(`https://jsonplaceholder.typicode.com/${this.companyName}`)
+//             .then((response) => response.json());
+//     }
+// };
+// console.log(users.getData());
+// function getCached(getData) {
+//     let cache = new Map();
 
-    return function (key) {
-        if (cache.has(key)) {
-            console.log(cache.get(key));
-            return cache.get(key);
-        } else {
-            let res = getData.call(this);// this ni qaytarib qoydik;
-            return cache.set(key, res);
-        }
-    }
-}
+//     return function (key) {
+//         if (cache.has(key)) {
+//             console.log(cache.get(key));
+//             return cache.get(key);
+//         } else {
+//             let res = getData.call(this);// this ni qaytarib qoydik;
+//             return cache.set(key, res);
+//         }
+//     }
+// }
 
-// users.getData = getCached(users.getData)
-users.getData() // Bu qism buttonga biriktirilishi kerak aslida;
+// // users.getData = getCached(users.getData)
+// users.getData() // Bu qism buttonga biriktirilishi kerak aslida;
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+// gettter and setter:
 
+let users = {
+    name: 'Akobir',
+    lastName: 'Ziyodullayev',
+    get fullName() {
+        return `${this.name} ${this.lastName}`;
+    },
+    set fullName({ name, lastName }) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+};
+// setter orqali fullName ga qiymat biriktirilmoqda:
+users.fullName = { name: 'Zuhriddin', lastName: 'Bahriddinov' };
 
+// console.log(users.fullName);
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// __proto__ bilan ishlash:
+
+let user1 = { name: 'Akobir' };
+// let user2 = { title: 'Zuhriddin' };
+
+user1.__proto__ = user2;
+
+console.log(user1.title); // Zuhriddin;
+
+user1.__proto__ = null;
+
+console.log(user1.title); // undefined;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// prototypega method yozish:
+
+let user1 = {
+    name: 'Akobir',
+    lastName: 'Ziyodullayev',
+};
+
+let user2 = {
+    name: 'Asilbek',
+    lastName: 'Boysoatov',
+};
+
+Object.prototype.fullName = function () {
+    console.log(this.name, this.lastName);
+}
+
+user1.fullName(); // Akobir Ziyodullayev;
+user2.fullName(); // Boysoatov Asilbek;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
